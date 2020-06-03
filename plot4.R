fb886d8ad55b59cda18286611e3c9f6e7b0c1e24
Aryan-2001 <f20180017@pilani.bits-pilani.ac.in>
2020-06-03 18:06:17 +0530

    done

--- plot4.R
+++ plot4.R
@@ -13,7 +13,7 @@ NEI <- NEI[NEI$SCC %in% coal_class_code$SCC, ]
 data1 <- tapply(NEI$Emissions , NEI$year , sum)
 data1 <- data.frame(Emissions = data1, year = row.names(data1))
 
-plot("plot4.png")
+png("plot4.png")
 plot(y=data1$Emissions , x=data1$year , col ="green" , type="l" , xlab = "year" ,ylab = "total emission")
 title("Total emmision by coal related sources")
 dev.off()
